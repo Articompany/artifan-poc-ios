7809a9c2afe6aa969a94f1561977fca07bebad8c
Victor Castro <victorcastro.io@icloud.com>
2024-08-10 19:30:08 -0500

    Initial Commit

---
+++ Artifan-POC/Artifan_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artifan_POCApp.swift
+//  Artifan-POC
+//
+//  Created by Victor Castro on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Artifan_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
